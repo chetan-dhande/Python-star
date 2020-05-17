f29a9831223f8e293177fdf364d3145e9ac8dff6
Rahul4269 <63596664+Rahul4269@users.noreply.github.com>
2020-05-17 20:54:45 +0530

    Add files via upload

---
+++ break continou pass.py
@@ -0,0 +1,21 @@
+for val in "Impetas":
+    if val=="e":
+     break
+    print(val,end="")
+
+print()
+
+for val in "Impetas":
+    if val=="e":
+        continue
+        print(val)
+    else:
+     print(val,end="")
+print()
+
+for val in"Impetas":
+    if val=="e":
+        pass
+        print(val)
+    else:
+        print(val,end="")
