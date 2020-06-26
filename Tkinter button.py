f4cc12d32896be5fafd2152ddf4842da282f78cf
Rahul4269 <63596664+Rahul4269@users.noreply.github.com>
2020-06-26 21:30:59 +0530

    Add files via upload

---
+++ Tkinter button.py
@@ -0,0 +1,8 @@
+from tkinter import *
+r=Tk()
+r.configure(background="orange")
+Button1=Button(r,text="SAVE",activebackground="green")
+Button2=Button(r,text="OK",activeforeground="green")
+Button1.pack()
+Button2.pack()
+r.mainloop()
