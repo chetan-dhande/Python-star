fe318407f03ae5cfb3d3b126def87087e0f0102e
Rahul4269 <63596664+Rahul4269@users.noreply.github.com>
2020-07-07 18:35:26 +0530

    Add files via upload

---
+++ entry1.py
@@ -0,0 +1,8 @@
+from tkinter import *
+r=Tk()
+e1=Entry(r,selectforeground="blue",selectbackground="yellow")
+e2=Entry(r,selectforeground="green",selectbackground="red")
+
+e1.pack()
+e2.pack()
+r.mainloop()
